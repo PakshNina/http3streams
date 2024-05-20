2bd81b451f66a4c997f9ad6ac42bb33e61343de5
Nina Pakshina <nina.pakshina@lenta.com>
2024-05-20 10:38:05 +0300

    Server and client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"crypto/tls"
+	"net/http"
 	"time"
 
 	"github.com/quic-go/quic-go"
 	"github.com/quic-go/quic-go/http3"
+	"github.com/quic-go/quic-go/quicvarint"
+	log "github.com/sirupsen/logrus"
 )
 
 const (
@@ -13,7 +16,16 @@ const (
 )
 
 func main() {
+	c := NewClient()
 
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go c.CreateStream(link, "Сообщение серверу №1", ch1)
+	go c.CreateStream(link, "Сообщение серверу №2", ch2)
+
+	<-ch1
+	<-ch2
 }
 
 type client struct {
@@ -30,4 +42,43 @@ func NewClient() *client {
 		},
 	}
 	return &client{rt: rt}
+}
+
+func (c *client) CreateStream(link, message string, ch chan string) {
+	req, err := http.NewRequest("GET", link, nil)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	rtOpt := http3.RoundTripOpt{DontCloseRequestStream: true}
+	res, err := c.rt.RoundTripOpt(req, rtOpt)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	log.Infof("Client received status %d", res.StatusCode)
+
+	// Открыли стрим
+	str := res.Body.(http3.HTTPStreamer).HTTPStream()
+	log.Infof("Stream was opened with id=%d", str.StreamID())
+
+	// Отправили данные на сервер
+	bytesToSend := []byte(message)
+	bytes := quicvarint.Append([]byte{}, uint64(len(bytesToSend)))
+	bytes = append(bytes, bytesToSend...)
+	if _, err = str.Write(bytes); err != nil {
+		log.Error(err)
+		return
+	}
+	log.Infof("Data to server was sent on str id=%d", str.StreamID())
+
+	// Получаем данные от сервера
+	lengthOfData, err := quicvarint.Read(quicvarint.NewReader(str))
+	bytesToReceive := make([]byte, lengthOfData)
+	if _, err = str.Read(bytesToReceive); err != nil {
+		log.Error(err)
+	}
+	log.Infof("Client received data '%s' from server on stream id=%d", string(bytesToReceive), str.StreamID())
+	str.CancelRead(quic.StreamErrorCode(quic.NoError))
+	ch <- string(bytesToReceive)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/quic-go/quic-go"
 	"github.com/quic-go/quic-go/http3"
+	"github.com/quic-go/quic-go/quicvarint"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -15,7 +18,8 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/stream", HandleStream)
 	srv := http3.Server{
-		Addr: addr,
+		Addr:    addr,
+		Handler: mux,
 	}
 	if err := srv.ListenAndServeTLS("cert.pem", "key.pem"); err != nil {
 		log.Error(err)
@@ -27,4 +31,33 @@ func HandleStream(w http.ResponseWriter, req *http.Request) {
 	w.(http.Flusher).Flush()
 
 	// Обработка стрима
+	str := req.Body.(http3.HTTPStreamer).HTTPStream()
+	log.Infof("Stream with id=%d from client was accepted", str.StreamID())
+	go readAndWrite(str)
+}
+
+func readAndWrite(str quic.Stream) {
+	// Считываем данные от клиента
+	lengthOfData, err := quicvarint.Read(quicvarint.NewReader(str))
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	bytes := make([]byte, lengthOfData)
+	if _, err = str.Read(bytes); err != nil {
+		log.Error(err)
+		return
+	}
+	log.Infof("Client sent data '%s' from stream id=%d", string(bytes), str.StreamID())
+
+	// Отправляем данные клиенту
+	message := []byte(fmt.Sprintf("Ответ сервера на стриме id=%d", str.StreamID()))
+	bytesToSend := quicvarint.Append([]byte{}, uint64(len(message)))
+	bytesToSend = append(bytesToSend, message...)
+
+	if _, err = str.Write(bytesToSend); err != nil {
+		log.Error(err)
+		return
+	}
+	<-str.Context().Done()
 }
